2ebd92ed5aa10852ad31bfe74e6ce2917d7a7313
William <williamrpelton@gmail.com>
2020-05-08 01:32:25 -0500

    Token and password protected feature settings system

---
+++ server/models/appSettings.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const settingSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  value: { type: Object, required: true },
+  prevName: String,
+  prevValue: Object,
+  createdOn: { type: Date, default: Date.now },
+  createdBy: { type: String, required: true },
+  modifiedOn: Date,
+  modifiedBy: String,
+});
+
+settingSchema.methods.updateSetting = async function (name, value, user) {
+  if (value && value !== this.value) {
+    this.prevValue = this.value;
+    this.value = value;
+    this.modifiedOn = Date.now();
+    this.modifiedBy = user;
+  }
+  if (name && name !== this.name) {
+    this.prevName = this.name;
+    this.name = name;
+    this.modifiedOn = Date.now();
+    this.modifiedBy = user;
+  }
+  await this.save();
+  return this;
+};
+
+module.exports = mongoose.model("Setting", settingSchema);
